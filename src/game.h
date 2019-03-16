338e0190a1906f477cc264ef4755c69dacafc953
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 17:41:41 +0100

    update game header

--- src/game.h
+++ src/game.h
@@ -9,7 +9,7 @@ typedef struct Game {
 
 game_t* game_create(SDL_Renderer* renderer);
 
-void game_update(game_t* game, const float delta_time);
+void game_update(game_t* game, const float delta_time, const float global_time, const uint64_t frame_count);
 void game_render(const game_t* game);
 
 void game_event(game_t* game, const SDL_Event event);
