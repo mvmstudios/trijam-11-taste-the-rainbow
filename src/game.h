48979a98c3927949aa9a2060d2de425f1130cc84
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 16:51:15 +0100

    basic game header

---
+++ src/game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+#include <SDL2/SDL.h>
+
+typedef struct Game {
+    SDL_Renderer* renderer;
+} game_t;
+
+game_t* game_create(SDL_Renderer* renderer);
+
+void game_update(game_t* game, const float delta_time);
+void game_render(const game_t* game);
+
+void game_event(game_t* game, const SDL_Event event);
+void game_keyboard_input(game_t* game, const Uint8* const keyboard_state);
+
+#endif
