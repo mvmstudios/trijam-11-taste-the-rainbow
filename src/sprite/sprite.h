dd7f5d5dae5cc3356e15c6084554fecd3a8c1250
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 18:38:45 +0100

    add renderer to sprite

--- src/sprite/sprite.h
+++ src/sprite/sprite.h
@@ -3,11 +3,13 @@
 
 #include <stdbool.h>
 
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
 #include "../math/mvmath.h"
 
 typedef struct Sprite {
+    SDL_Renderer* renderer;
     SDL_Texture* sprite_texture;
 
     bool play_animation;
@@ -19,7 +21,7 @@ typedef struct Sprite {
     vec2i_t current_animation_range;
 } sprite_t;
 
-sprite_t* sprite_create(const char* path_to_sprite, bool play_animation, vec2i_t dimension, int start_index, vec2i_t animation_range);
+sprite_t* sprite_create(SDL_Renderer* renderer, const char* path_to_sprite, bool play_animation, vec2i_t dimension, int start_index, vec2i_t animation_range);
 // void sprite_destroy();
 
 void sprite_update(sprite_t* sprite);
