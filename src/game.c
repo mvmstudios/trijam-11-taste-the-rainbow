a72e08be775b4573fec706cb3a46392ab8212840
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 16:52:12 +0100

    include functions from header

---
+++ src/game.c
@@ -0,0 +1,21 @@
+#include "game.h"
+
+game_t* game_create(SDL_Renderer* renderer) {
+    
+}
+
+void game_update(game_t* game, const float delta_time) {
+
+}
+
+void game_render(const game_t* game) {
+
+}
+
+void game_event(game_t* game, const SDL_Event event) {
+
+}
+
+void game_keyboard_input(game_t* game, const Uint8* const keyboard_state) {
+
+}
