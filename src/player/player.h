4f9af111e31c00b8ef5dbe1442250869d7ffee4b
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 18:08:25 +0100

    create player header

---
+++ src/player/player.h
@@ -0,0 +1,21 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+#include <stdlib.h>
+
+#include "../game.h"
+#include "../math/mvmath.h"
+
+typedef struct Player {
+    game_t* game;
+
+    vec2i_t position;
+} player_t;
+
+player_t* player_create(Game* game, vec2i_t start_position);
+// void player_destroy();
+
+void player_update(player_t* player);
+void player_render(const player_t* player);
+
+#endif
