e88da0d185c7b854b909cc369be9c805a77f2182
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 22:57:07 +0100

    add game as attr

--- src/skittle/skittle.h
+++ src/skittle/skittle.h
@@ -1,18 +1,21 @@
 #ifndef SKITTLE_H_
 #define SKITTLE_H_
 
+#include "../game.h"
 #include "../math/mvmath.h"
 #include "../sprite/sprite.h"
 #include "../hitbox/hitbox.h"
 
 typedef struct Skittle {
+    game_t* game;
+
     vec2i_t position;
 
     sprite_t sprite;
     hitbox_t hitbox;
 } skittle_t;
 
-skittle_t* skittle_create(vec2i_t init_pos);
+skittle_t* skittle_create(game_t* game, vec2i_t init_pos);
 
 void skittle_update(skittle_t* skittle, float global_time);
 
