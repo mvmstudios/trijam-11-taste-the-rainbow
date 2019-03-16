469f0597d3023b4852aa11afbcbd34ede5191ba4
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 17:50:09 +0100

    game_create function implementation

--- src/game.c
+++ src/game.c
@@ -1,15 +1,19 @@
 #include "game.h"
 
 game_t* game_create(SDL_Renderer* renderer) {
-    
+    game_t* game = malloc(sizeof(game));
+
+    game->renderer = renderer;
+
+    return game;
 }
 
 void game_update(game_t* game, const float delta_time, const float global_time, const uint64_t frame_count) {
-
+    
 }
 
 void game_render(const game_t* game) {
-
+    SDL_RenderClear(game->renderer);
 }
 
 void game_event(game_t* game, const SDL_Event event) {
