eacc03c0bcbea07ea35b058ccc226fc1e22e827a
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 22:49:58 +0100

    create skittle header

---
+++ src/skittle/skittle.h
@@ -0,0 +1,21 @@
+#ifndef SKITTLE_H_
+#define SKITTLE_H_
+
+#include "../math/mvmath.h"
+#include "../sprite/sprite.h"
+#include "../hitbox/hitbox.h"
+
+typedef struct Skittle {
+    vec2i_t position;
+
+    sprite_t sprite;
+    hitbox_t hitbox;
+} skittle_t;
+
+skittle_t* skittle_create();
+
+void skittle_update(skittle_t* skittle, uint64_t global_time);
+
+void skittle_render(skittle_t* skittle);
+
+#endif
