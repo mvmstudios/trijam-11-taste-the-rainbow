a2510da928cff9959df9cc13c241aa8867ba45d9
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 16:56:01 +0100

    init sdl and create window

--- src/main.c
+++ src/main.c
@@ -3,4 +3,13 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char** argv) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER))
+        return -1;
+
+    SDL_Window* window = SDL_CreateWindow("Trijam 11", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    if (!window) {
+        SDL_Quit();
+        return -1;
+    }
+
 }
