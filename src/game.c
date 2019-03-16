ff2b0a67aa101e7f7ad08275bd6a84aaae93a0fc
Maurice Schmidt <mvmogo@gmail.com>
2019-03-16 17:41:58 +0100

    function changes

--- src/game.c
+++ src/game.c
@@ -4,7 +4,7 @@ game_t* game_create(SDL_Renderer* renderer) {
     
 }
 
-void game_update(game_t* game, const float delta_time) {
+void game_update(game_t* game, const float delta_time, const float global_time, const uint64_t frame_count) {
 
 }
 
