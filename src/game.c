b609f546a5e4a55e00b1eb514c12bc9d73ba689e
Maurice Schmidt <mvmogo@gmail.com>
2019-03-17 02:45:13 +0100

    skittles now dropping

--- src/game.c
+++ src/game.c
@@ -9,7 +9,7 @@
 bool debug = false;
 
 floor_t* _floor;
-skittle_t* skittles[1028];
+skittle_t** skittles;
 
 int current_skittle_index = 0;
 
@@ -26,14 +26,14 @@ typedef struct SkittleDropper {
 skittle_dropper_t* skittle_dropper_create(game_t* game, vec2i_t init_pos) {
     skittle_dropper_t* dropper = malloc(sizeof(skittle_dropper_t));
 
-    dropper->velocity = (vec2i_t) { 10, 0 };
+    dropper->velocity = (vec2i_t) { 3, 0 };
     dropper->game = game;
-    dropper->hitbox = hitbox_create(init_pos.x, init_pos.y, 10, 10);
+    dropper->hitbox = hitbox_create(init_pos.x, init_pos.y, 50, 50);
 
     return dropper;
 }
 
-void skittle_dropper_update(skittle_dropper_t* dropper) {
+void skittle_dropper_update(skittle_dropper_t* dropper, uint64_t frame_count) {
     dropper->hitbox->x += dropper->velocity.x;
 
     if (dropper->hitbox->x > window_width - dropper->hitbox->width) {
@@ -46,11 +46,16 @@ void skittle_dropper_update(skittle_dropper_t* dropper) {
         dropper->velocity.x = -dropper->velocity.x;
     }
 
+    // only works properly with vsync
+    if (frame_count % 60 * 2 == 0) {
+        skittles[current_skittle_index++] = skittle_create(dropper->game, (vec2i_t) {dropper->hitbox->x, dropper->hitbox->y});
+    }
+
 }
 
 void skittle_dropper_render(skittle_dropper_t* dropper) {
     if (debug)
-        hitbox_render(dropper->hitbox, dropper->game->renderer);
+        hitbox_render_colored(dropper->hitbox, dropper->game->renderer, (vec4i_t) { 250, 0, 250, 255 }, (vec4i_t) { 125, 255, 255, 100 });
 }
 
 skittle_dropper_t* skittle_dropper;
@@ -61,24 +66,26 @@ game_t* game_create(SDL_Renderer* renderer) {
     game->renderer = renderer;
     game->player = player_create(game, (vec2i_t) {0, 0});
 
+    skittles = malloc(sizeof(skittle_t*) * 1024);
+
     _floor = floor_create(game);
     skittle_dropper = skittle_dropper_create(game, (vec2i_t) {0, 0});
 
     return game;
 }
 
+void game_destroy(game_t* game) {
+    free(skittles);
+}
+
 void game_update(game_t* game, const float delta_time, const float global_time, const uint64_t frame_count) {
     player_update(game->player);
     floor_update(_floor);
 
-    skittle_dropper_update(skittle_dropper);
+    skittle_dropper_update(skittle_dropper, frame_count);
 
-    // i don't like this!
-    //if (frame_count % 200 == 0 && current_skittle_index <= 128 - 2)
-        //skittles[current_skittle_index++] = skittle_create(game, (vec2i_t) {0, 10});
-
-    //for (int i = 0; i < current_skittle_index; i++)
-       // skittle_update(skittles[i], global_time);
+    for (int i = 0; i < current_skittle_index; i++)
+        skittle_update(skittles[i], global_time);
 }
 
 void game_render(const game_t* game) {
@@ -90,8 +97,8 @@ void game_render(const game_t* game) {
 
     skittle_dropper_render(skittle_dropper);
 
-   // for (int i = 0; i < current_skittle_index; i++)
-       // skittle_render(skittles[i]);
+    for (int i = 0; i < current_skittle_index; i++)
+        skittle_render(skittles[i]);
 }
 
 void game_event(game_t* game, const SDL_Event event) {
