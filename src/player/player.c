7da68fff460920e974efc33c922b330601eabcf9
Maurice Schmidt <mvmogo@gmail.com>
2019-03-17 04:14:54 +0100

    play pick up sound

--- src/player/player.c
+++ src/player/player.c
@@ -5,11 +5,14 @@
 #include "../game.h"
 #include "../skittle/skittle.h"
 #include "../sprite/sprite.h"
+#include "SDL2/SDL_mixer.h"
 
 extern int window_width;
 extern int window_height;
 
 extern bool debug;
+
+Mix_Chunk* pick_up_sound;
 
 player_t* player_create(game_t* game, vec2i_t start_position) {
     player_t* player = malloc(sizeof(player_t));
@@ -25,6 +28,8 @@ player_t* player_create(game_t* game, vec2i_t start_position) {
     player->sprite->update_each_nth_frame = 10;
 
     player->hitbox = hitbox_create(player->position.x, player->position.y, player->sprite->size.x - 20, player->sprite->size.y);
+
+    pick_up_sound = Mix_LoadWAV("assets/sound/pick.wav");
 
     return player;
 }
@@ -73,11 +78,11 @@ void player_keyboard_input(player_t* player, const Uint8* const keyboard_state) 
         player->facing = PLAYER_FACING_LEFT;
         player_move_to(player, (vec2i_t) {-4.5, 0}); 
         player->sprite->current_animation_range = (vec2i_t) {0, 2};
-    } else if (keyboard_state[SDL_SCANCODE_W] || keyboard_state[SDL_SCANCODE_UP]) {
+    } /*else if (keyboard_state[SDL_SCANCODE_W] || keyboard_state[SDL_SCANCODE_UP]) {
         player->facing = PLAYER_FACING_UP;
         player->sprite->current_animation_range = (vec2i_t) {3, 5};
         player->sprite->current_sprite_index = 3;
-    }
+    }*/
 }
 
 void player_move_to(player_t* player, vec2i_t position) {
@@ -88,4 +93,7 @@ void player_move_to(player_t* player, vec2i_t position) {
 void player_eat_skittle(player_t* player, skittle_t* skittle) {
     skittle->disabled = true;
     player->score++;
+
+    player->sprite->current_sprite_index = 3;
+    Mix_PlayChannel(-1, pick_up_sound, 0);
 }
