3f30bd9d303b5ce6217757c27074ab91fd861a43
Maurice Schmidt <mvmogo@gmail.com>
2019-03-17 02:37:47 +0100

    game destory prot

--- src/game.h
+++ src/game.h
@@ -12,6 +12,7 @@ typedef struct Game {
 } game_t;
 
 game_t* game_create(SDL_Renderer* renderer);
+void game_destroy(game_t* game);
 
 void game_update(game_t* game, const float delta_time, const float global_time, const uint64_t frame_count);
 void game_render(const game_t* game);
