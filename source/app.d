3b98f1f29a29dc161dfdf4639be5ba0c585a79ff
Maurice Schmidt <mvmogo@gmail.com>
2019-03-20 20:20:59 +0100

    create window

--- source/app.d
+++ source/app.d
@@ -1,6 +1,20 @@
-import std.stdio;
-
-void main()
+void main() 
 {
-	writeln("Edit source/app.d to start your project.");
+	import std.stdio: writeln;
+	import bindbc.sdl;
+
+	SDLSupport ret = loadSDL();
+	if (ret != sdlSupport) {
+		if (ret == SDLSupport.noLibrary) {
+			writeln("No library");
+		}
+
+		if (ret == SDLSupport.badLibrary) {
+			writeln("Wrong library");
+		}
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Window Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+
+	SDL_Delay(5000);
 }
