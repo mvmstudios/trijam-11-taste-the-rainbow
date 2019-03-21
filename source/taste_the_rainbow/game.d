734000eafd42a90ee4588874e83595aa500b57ed
Maurice Schmidt <mvmogo@gmail.com>
2019-03-21 19:16:35 +0100

    create player and render to screen

--- source/taste_the_rainbow/game.d
+++ source/taste_the_rainbow/game.d
@@ -1,14 +1,22 @@
 module taste_the_rainbow.game;
 
+import std.container;
 import taste_the_rainbow.rendering.renderable: IRenderable;
+import taste_the_rainbow.entity.entity;
+
 final class Game : IRenderable {
 
     private:
         static Game _instance;
 
+        IRenderable renderable;
+
+
     public:
         this() {
             _instance = this;
+
+            renderable = new Player;
         }
 
         static Game instance() {
@@ -19,13 +27,15 @@ final class Game : IRenderable {
         }
 
         override void update(float deltaTime) {
-            
+            renderable.update(deltaTime);
         }
 
         import bindbc.sdl: SDL_RenderClear, SDL_SetRenderDrawColor;
         override void render(SDL_Renderer* renderer) {
             SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
             SDL_RenderClear(renderer);
+
+            renderable.render(renderer);
         }
     
 }
